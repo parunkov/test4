ba5ed2360c40db09a4ea7ed4ae15f7a914f07b2d
parunkov <parunkov@gmail.com>
2020-09-04 14:49:29 +0500

    Updates

---
+++ precache-manifest.ddab656ed91e6f9215be047b087522b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e406436b8fa89ed563babf5947d9b2",
+    "url": "/test4/index.html"
+  },
+  {
+    "revision": "83bd3a36b5636f7de231",
+    "url": "/test4/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "4dd06feb4e04dde42e05",
+    "url": "/test4/static/css/main.c04186d3.chunk.css"
+  },
+  {
+    "revision": "83bd3a36b5636f7de231",
+    "url": "/test4/static/js/2.c618be15.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/test4/static/js/2.c618be15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd06feb4e04dde42e05",
+    "url": "/test4/static/js/main.eea88d6e.chunk.js"
+  },
+  {
+    "revision": "4a511faeb1a6fe8979d0",
+    "url": "/test4/static/js/runtime-main.a2434916.js"
+  }
+]);
