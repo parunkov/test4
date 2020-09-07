223d63f1a39daac3773d78c48aa35219759383d3
parunkov <parunkov@gmail.com>
2020-09-07 13:54:06 +0500

    Исправил верстку

--- src/components/common/GitHubLink/GitHubLink.jsx
+++ src/components/common/GitHubLink/GitHubLink.jsx
@@ -3,7 +3,7 @@ import './GitHubLink.scss';
 
 const GitHubLink = () => {
 	return(
-		<a href="https://github.com/parunkov/test1" className="git-hub-link" target="_blank" rel="noopener noreferrer">@parunkov</a>
+		<a href="https://github.com/parunkov/test4" className="git-hub-link" target="_blank" rel="noopener noreferrer">@parunkov</a>
 	)
 }
 export default GitHubLink;
